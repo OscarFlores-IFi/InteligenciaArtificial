1738043764ec829bc7917de9b2da0d35d209630e
Unknown <if715029@gmail.com>
2019-10-29 19:57:10 -0600

    Limpieza de base de datos para CIFI

---
+++ Trabajos y Exámenes/CIFI V/Digitt.py
@@ -0,0 +1,42 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from mylib import mylib
+from sklearn.preprocessing import PolynomialFeatures
+
+
+#%%
+Data = pd.read_csv('Digitt.csv',index_col='id')
+data = Data.iloc[:,0:20]
+#plt.hist(data.age,bins=20) # Edad de las personas que piden el servicio
+#plt.hist(data.score,bins=20) # el 'score' esta distribuido de forma casi plana. 
+#pd.value_counts(data.status).plot(kind='bar') # Se acepta casi a la mitad de las personas que solicitan el servicio
+reporte = mylib.dqr(data)
+#%% Para Status. Accepted vs Rejected. 
+# Limpieza de base de datos. 
+stat = data.drop(columns=['reasson', 'payment_status','created_at'])
+sts = mylib.dqr(stat)
+
+
+Quant = stat.loc[:,['income', 'age', 'score', 'total_debt', 'amount_to_be_lend']]
+Dummies = stat.loc[:,['gender', 'housing_type','occupancy_type','level_of_studies','state','marital_status']]
+Extras = stat.loc[:,['icc', 'tenure']]
+Y = pd.get_dummies(stat.status)
+
+#%% Creamos Dummies.
+# uniques 
+n = Dummies.shape[1]
+uq = np.zeros(n+1)
+for i in np.arange(n):
+    uq[i+1] = len(pd.unique(Dummies.iloc[:,i]))-1
+uq = uq.cumsum().astype(int)
+
+#llenamos matriz con dummies. 
+mat = pd.get_dummies(Dummies.iloc[:,0])
+for i in np.arange(1,n):
+    mat = mat.join(pd.get_dummies(Dummies.iloc[:,i]))
+
+Dummies = mat
+#%% Unimos todas las variables y las exportamos a un csv 'limpio'
+limpio = Quant.join(Dummies).join(Extras).join(Y)
+limpio.to_csv('D_limpio.csv')
---
+++ Trabajos y Exámenes/CIFI V/mylib.py
@@ -0,0 +1,23 @@
+class mylib:
+    def dqr(data):
+        import pandas as pd        
+        
+        columns = pd.DataFrame(list(data.columns.values),columns=['Nombres'],index=list(data.columns.values))
+        d_types = pd.DataFrame(data.dtypes,columns=['tipo'])
+        missing_val = pd.DataFrame(data.isnull().sum(),columns=['Valores perdidos'])
+        present_values = pd.DataFrame(data.count(),columns=['Valores presentes'])
+        unique_values = pd.DataFrame(data.nunique(),columns=['Valores unicos'])
+        
+        
+        min_values = pd.DataFrame(columns=['Min'])
+        max_values = pd.DataFrame(columns=['Max'])
+        for col in list(data.columns.values):
+            try:
+                min_values.loc[col] = [data[col].min()]
+                max_values.loc[col] = [data[col].max()]
+            except:
+                pass
+        
+        reporte_calidad_datos = columns.join(d_types).join(missing_val).join(present_values).join(unique_values).join(min_values).join(max_values)
+        
+        return (reporte_calidad_datos)
