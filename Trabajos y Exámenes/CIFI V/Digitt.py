72029839119e840d65cf5a1a6d03acc89f0465e3
OscarFlores-IFi <41029452+OscarFlores-IFi@users.noreply.github.com>
2019-11-01 10:46:03 -0600

    rate0 y amount0

--- Trabajos y Exámenes/CIFI V/Digitt.py
+++ Trabajos y Exámenes/CIFI V/Digitt.py
@@ -8,6 +8,7 @@ from sklearn.preprocessing import PolynomialFeatures
 #%%
 Data = pd.read_csv('Digitt.csv',index_col='id')
 data = Data.iloc[:,0:20]
+data = data.join(Data.rate_0).join(Data.loan_amount_0)
 #plt.hist(data.age,bins=20) # Edad de las personas que piden el servicio
 #plt.hist(data.score,bins=20) # el 'score' esta distribuido de forma casi plana. 
 #pd.value_counts(data.status).plot(kind='bar') # Se acepta casi a la mitad de las personas que solicitan el servicio
@@ -18,9 +19,8 @@ stat = data.drop(columns=['reasson', 'payment_status','created_at'])
 sts = mylib.dqr(stat)
 
 
-Quant = stat.loc[:,['income', 'age', 'score', 'total_debt', 'amount_to_be_lend']]
+Quant = stat.loc[:,['income', 'age', 'score','icc', 'tenure','rate', 'total_debt', 'amount_to_be_lend','loan_amount_0','rate_0']]
 Dummies = stat.loc[:,['gender', 'housing_type','occupancy_type','level_of_studies','state','marital_status']]
-Extras = stat.loc[:,['icc', 'tenure']]
 Y = pd.get_dummies(stat.status)
 
 #%% Creamos Dummies.
@@ -30,7 +30,9 @@ uq = np.zeros(n+1)
 for i in np.arange(n):
     uq[i+1] = len(pd.unique(Dummies.iloc[:,i]))-1
 uq = uq.cumsum().astype(int)
-
+#2630
+#prestadero
+#
 #llenamos matriz con dummies. 
 mat = pd.get_dummies(Dummies.iloc[:,0])
 for i in np.arange(1,n):
@@ -38,5 +40,5 @@ for i in np.arange(1,n):
 
 Dummies = mat
 #%% Unimos todas las variables y las exportamos a un csv 'limpio'
-limpio = Quant.join(Dummies).join(Extras).join(Y)
+limpio = Quant.join(Dummies).join(Y)
 limpio.to_csv('D_limpio.csv')
