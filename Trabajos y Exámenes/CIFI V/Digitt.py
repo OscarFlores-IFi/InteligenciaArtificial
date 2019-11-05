1bf4ed5231f7a7ad92d40c5058f7845d0b0b66de
OscarFlores-IFi <41029452+OscarFlores-IFi@users.noreply.github.com>
2019-11-05 17:38:34 -0600

    elimina el estado, rellena algunos datos

--- Trabajos y Exámenes/CIFI V/Digitt.py
+++ Trabajos y Exámenes/CIFI V/Digitt.py
@@ -9,6 +9,7 @@ from sklearn.preprocessing import PolynomialFeatures
 Data = pd.read_csv('Digitt.csv',index_col='id')
 data = Data.iloc[:,0:20]
 data = data.join(Data.rate_0).join(Data.loan_amount_0)
+data.amount_to_be_lend[np.isnan(data.amount_to_be_lend)] = data.total_debt[np.isnan(data.amount_to_be_lend)] # llenamos todos los nan faltantes en amount_to_be_lend con los total_debt (ya que usualmente asi se comportan.)
 #plt.hist(data.age,bins=20) # Edad de las personas que piden el servicio
 #plt.hist(data.score,bins=20) # el 'score' esta distribuido de forma casi plana. 
 #pd.value_counts(data.status).plot(kind='bar') # Se acepta casi a la mitad de las personas que solicitan el servicio
@@ -18,9 +19,9 @@ reporte = mylib.dqr(data)
 stat = data.drop(columns=['reasson', 'payment_status','created_at'])
 sts = mylib.dqr(stat)
 
-
-Quant = stat.loc[:,['income', 'age', 'score','icc', 'tenure','rate', 'total_debt', 'amount_to_be_lend','loan_amount_0','rate_0']]
-Dummies = stat.loc[:,['gender', 'housing_type','occupancy_type','level_of_studies','state','marital_status']]
+Quant = stat.loc[:,['income', 'age', 'score','icc', 'tenure','rate', 'total_debt', 'amount_to_be_lend']]
+#Dummies = stat.loc[:,['gender', 'housing_type','occupancy_type','level_of_studies','state','marital_status']]
+Dummies = stat.loc[:,['gender', 'housing_type','occupancy_type','level_of_studies','marital_status']]
 Y = pd.get_dummies(stat.status)
 
 #%% Creamos Dummies.
